31b098bfa861762bb4c16750b5c4c6df2a21c122
shoaibhere <shoaibakhtar321@icloud.com>
2025-03-21 00:40:41 +0500

    homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,13 +4,17 @@ import Contact from "./Component/Contact";
 import ProductGrid from "./Component/OurProducts";
 import Benefit from "./Component/Features";
 import HeroSection from "./Component/HeroSection";
+import Message from "./Component/Message";
+import Categories from "./Component/Categories";
 
 export default function Home() {
     return (
-        <div className="bg-[#FAF0E6]">  
+        <div className="bg-light">  
             <HeroSection  />
             <Benefit className="md:py-20 py-10" />
-            {/* <ProductGrid/> */}
+            <ProductGrid/>
+            <Message />
+            <Categories/>
             <Contact/>
             <Newsletter  />
             <Footer/>
